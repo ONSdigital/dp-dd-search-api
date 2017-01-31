ecfb1be3fd33b9067b008b8f6a834580f8e832ed
Carl Hembrough <carlhuk@gmail.com>
2017-01-31 11:06:41 +0000

    Add generic body field to the document structure.

--- model/document.go
+++ model/document.go
@@ -2,8 +2,7 @@ package model
 
 // Document contains the base properties for all documents stored in Elastic Search.
 type Document struct {
-	ID    string `json:"id"`
-	Type  string `json:"type"`
-	Title string `json:"title"`
-	URL   string `json:"url"`
+	ID   string      `json:"id"`
+	Type string      `json:"type"`
+	Body interface{} `json:"body"`
 }
